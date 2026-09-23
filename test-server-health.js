4392d6173a849d6521c9906073bddc0c7761def0
domainex <connect.domainexpansion@gmail.com>
2026-09-24 00:30:28 +0530

    fix: resolve issue on portal

--- test-server-health.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-http.get("http://localhost:3000/login", (res) => {
-  process.exit(res.statusCode === 200 ? 0 : 1);
-}).on("error", (e) => {
-  console.error("HTTP error:", e.message);
-  process.exit(1);
-});
